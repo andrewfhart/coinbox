89de75070d416b21a3a993b9c9f7b85cf62d0ce4
Andrew Hart <hello@andrewfhart.com>
2015-02-22 10:06:09 -0800

    Refactor server-side code into src/server

---
+++ src/server/models/user.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var bcrypt = require('bcrypt');
+
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    username: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        User.hasOne(models.UserInfo);
+      },
+      hash: function(password, callback) {
+        bcrypt.genSalt(11, function (err, salt) {
+          if (err){
+            callback(err, null);
+          } else {
+            bcrypt.hash(password, salt, function(err, hash) {
+              if (err){
+                callback(err, null);
+              } else {
+                callback(null, hash.toString('hex'));
+              }
+            });
+          }
+        });
+      },
+      checkPassword: function(password, hash, callback) {
+        bcrypt.compare(password, hash, function (err, matched) {
+          callback(err, matched);
+        });
+      }
+    }
+  });
+
+  return User;
+};
