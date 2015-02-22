4dfeea3525ff12b651f65897153cc0861abe3cf9
Andrew Hart <hello@andrewfhart.com>
2015-02-22 10:05:49 -0800

    Add initial User router

---
+++ src/server/api/user/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var express    = require('express');
+var controller = require('./user');
+var router     = express.Router();
+
+router.post('/create', controller.create);
+router.post('/authenticate', controller.authenticate);
+router.get('/session', controller.session);
+router.post('/logout', controller.logout);
+
+module.exports = router;
