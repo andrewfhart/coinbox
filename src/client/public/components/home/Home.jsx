bd870a2ce12f4e577ef01698a65967a03cad40b7
Andrew Hart <hello@andrewfhart.com>
2015-02-22 23:08:19 -0800

    Add a simple bootstrap navbar and event handlers to main home view

--- src/client/public/components/home/Home.jsx
+++ src/client/public/components/home/Home.jsx
@@ -1,14 +1,44 @@
 var React  = require('react');
 var Flux   = require('react-flux');
 var Router = require('react-router');
+var Bootstrap = require('react-bootstrap');
+
 var {Route, RouteHandler, Link } = Router;
+var {Navbar, Nav, NavItem, DropdownButton, MenuItem} = Bootstrap;
 
 
 var Home = React.createClass({
+
+  mixins: [
+    Router.Navigation
+  ],
+
+  navigateHome: function (event) {
+    event.preventDefault();
+    this.transitionTo('/home');
+  },
+
+  navigateAccounts: function (event) {
+    event.preventDefault();
+    this.transitionTo('/accounts');
+  },
+
   render: function () {
     return (
       <div>
-        <h3>Welcome home!</h3>
+        <Navbar>
+          <Nav>
+            <NavItem eventKey={1} href="/home" onClick={this.navigateHome}>Home</NavItem>
+            <NavItem eventKey={2} href="/accounts" onClick={this.navigateAccounts}>Accounts</NavItem>
+            <DropdownButton eventKey={3} title="Dropdown">
+              <MenuItem eventKey="1">Action</MenuItem>
+              <MenuItem eventKey="2">Another action</MenuItem>
+              <MenuItem eventKey="3">Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey="4">Separated link</MenuItem>
+            </DropdownButton>
+          </Nav>
+        </Navbar>
         <RouteHandler/>
       </div>
     );
