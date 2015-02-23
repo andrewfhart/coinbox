4ff1721241a0f8f20b33b27029773bd9f76b27b3
Andrew Hart <hello@andrewfhart.com>
2015-02-22 16:12:30 -0800

    Add continuous monitoring to jsx-compilation gulp step

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,48 @@
 var gulp        = require('gulp');
-var browserify  = require('gulp-browserify');
-var react       = require('gulp-react');
-var plumber     = require('gulp-plumber');
+var debug       = require('gulp-debug');
+var source      = require('vinyl-source-stream');
+var browserify  = require('browserify');
+var watchify    = require('watchify');
+var reactify    = require('reactify');
 var concat      = require('gulp-concat');
+var plumber     = require('gulp-plumber');
 
 gulp.task('default', ['build-client', 'build-server']);
 gulp.task('build-client', ['compile-jsx', 'copy-index']);
 gulp.task('build-server', ['build-api']);
 
 gulp.task('compile-jsx', function(cb){
+
+  var bundler = browserify({
+    entries: ['./src/client/public/coinbox.jsx'],
+    transform: ['reactify'],
+    debug: true,
+    cache: {}, packageCache: {}, fullPaths: true
+  });
+
+  var watcher = watchify(bundler);
+
+  return watcher
+    .on('update', function (){  // When any files update...
+      var updateStart = Date.now();
+      console.log('Updating!');
+      watcher.bundle()
+        .pipe(source('coinbox.js'))
+        .pipe(gulp.dest('./release/public/'));
+      console.log('Updated.', Date.now() - updateStart  + 'ms');
+    })
+    .bundle()   // create the initial bundle when starting the task
+    .pipe(source('coinbox.js'))
+    .pipe(gulp.dest('./release/public/'));
+});
+
+/*
   return gulp.src([
-    './src/client/public/coinbox.jsx'
+    './src/client/public/components/**//*.jsx',
+    './src/client/public/coinbox.jsx',
+    './src/client/public/routes.jsx'
   ])
+  .pipe(debug())
   .pipe(plumber())
   .pipe(react({ addPragma: false }))
   .pipe(browserify({
@@ -21,6 +52,7 @@ gulp.task('compile-jsx', function(cb){
   .pipe(concat('coinbox.js'))
   .pipe(gulp.dest('./release/public/'));
 });
+*/
 
 gulp.task('copy-index', function (cb) {
   return gulp.src([
