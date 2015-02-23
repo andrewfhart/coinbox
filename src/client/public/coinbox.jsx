eeecf8ab5ea62a5801c9ca58b86e9ca058cc44c1
Andrew Hart <hello@andrewfhart.com>
2015-02-22 16:18:39 -0800

    Update main application class to initialize react-router

--- src/client/public/coinbox.jsx
+++ src/client/public/coinbox.jsx
@@ -1,76 +1,12 @@
-var React = require('react');
-var Flux  = require('react-flux');
+'use strict';
 
-var userStore   = require('./flux/stores/user');
-var userActions = require('./flux/actions/user');
+var React  = require('react');
+var Router = require('react-router');
+var routes = require('./routes.jsx');
+var Route  = Router.Route;
 
-var App = React.createClass({
-
-  mixins: [
-    userStore.mixin()
-  ],
-
-  getStateFromStores: function(){
-    console.log("App.getStateFromStores");
-    return {
-      user: userStore.state
-    };
-  },
-
-  login: function(){
-    var username = this.refs.username.getDOMNode().value;
-    var password = this.refs.password.getDOMNode().value;
-    userActions.login(username, password);
-    return false;
-  },
-
-  logout: function(){
-    userActions.logout();
-    return false;
-  },
-
-  render: function(){
-    if( !this.state.user.get('isAuth') ){
-      return this.renderLogin();
-    }
-    return this.renderHome();
-  },
-
-  renderHome: function(){
-    return (
-      <div>
-      <h3>Hello {this.state.user.get('data').username}!</h3>
-      <a href="#" onClick={this.logout}>Logout</a>
-      </div>
-    );
-  },
-
-  renderLogin: function(){
-    if( this.state.user.get('isLoggingIn') ){
-      return(<div>Logging in...</div>);
-    }
-    return(
-      <div>
-        <h3>LOGIN</h3>
-        Username: <input type="text" ref="username" /> <i>Leave empty to cause an error</i>
-        <br />
-        Password: <input type="password" ref="password" /> <i>Leave empty to cause an error</i>
-        <br />
-        <button onClick={this.login}>Click to login</button>
-        {this.renderLoginError()}
-      </div>
-    );
-  },
-
-  renderLoginError: function(){
-    if( !this.state.user.get('error') ){
-      return;
-    }
-    return (<div style={{color: 'brown'}}>{this.state.user.get('error')}</div>)
-  }
-
+document.addEventListener("DOMContentLoaded", function(event) {
+  Router.run(routes, Router.HistoryLocation, function (Handler, state) {
+    React.render(<Handler />, document.getElementById('__app'));
+  });
 });
-
-window.onload = function(){
-  React.render(<App />, document.getElementById('__app'));
-};
