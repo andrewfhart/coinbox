d656d2a9282a99bd1ff6d2cf861cb1abaee1a327
Andrew Hart <hello@andrewfhart.com>
2015-02-22 22:05:36 -0800

    Clean up the App component

--- src/client/public/components/App.jsx
+++ src/client/public/components/App.jsx
@@ -4,9 +4,10 @@ var React   = require('react');
 var Router  = require('react-router');
 var { Route, RouteHandler, Link } = Router;
 
+var userStore = require('../flux/stores/user');
+
 var App = React.createClass({
 
-  /*
   mixins: [
     userStore.mixin()
   ],
@@ -17,62 +18,16 @@ var App = React.createClass({
       user: userStore.state
     };
   },
-  */
 
   render: function(){
     return (
       <div>
-        <ol>
-          <li><Link to="home">Home</Link></li>
-          <li><Link to="login">Log In</Link></li>
-        </ol>
+        <h1>Coinbox</h1>
         <RouteHandler/>
       </div>
     );
-    /*
-    if( !this.state.user.get('isAuth') ){
-      return this.renderLogin();
-    }
-    return this.renderHome();
-    */
   }
 });
 
-  /*
-  renderHome: function(){
-    return (
-      <div>
-      <h3>Hello {this.state.user.get('data').username}!</h3>
-      <a href="#" onClick={this.logout}>Logout</a>
-      </div>
-    );
-  },
-
-  renderLogin: function(){
-    if( this.state.user.get('isLoggingIn') ){
-      return(<div>Logging in...</div>);
-    }
-    return(
-      <div>
-        <h3>LOGIN</h3>
-        Username: <input type="text" ref="username" /> <i>Leave empty to cause an error</i>
-        <br />
-        Password: <input type="password" ref="password" /> <i>Leave empty to cause an error</i>
-        <br />
-        <button onClick={this.login}>Click to login</button>
-        {this.renderLoginError()}
-      </div>
-    );
-  },
-
-  renderLoginError: function(){
-    if( !this.state.user.get('error') ){
-      return;
-    }
-    return (<div style={{color: 'brown'}}>{this.state.user.get('error')}</div>)
-  }
-
-});
-*/
 
 module.exports = App;
